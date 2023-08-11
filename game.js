d8b1b5c7d4e0ed917be13c5f2ad12d20e801cd39
Jeidsgn <jeirm8228@gmail.com>
2023-08-11 16:29:08 -0500

    point

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ const config = {
   
   function create() {
     circles = this.add.group();  // Crear un grupo para almacenar los círculos
-      
+  
     // Agregar un botón para activar/desactivar el dibujo
     const toggleButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
       .setInteractive()
@@ -34,28 +34,25 @@ const config = {
   
   function toggleDrawing() {
     isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
-    
+  
     // Cambiar el texto del botón según el estado del dibujo
     this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
-    
-    // Eliminar el listener anterior si existe
-    this.input.off('pointerdown', createCircle.bind(this));
-    
-    // Agregar el listener solo si el dibujo está activado
+  
+    // Si el dibujo está habilitado, configurar el evento de clic en el contenedor
     if (isDrawingEnabled) {
-      this.input.on('pointerdown', createCircle.bind(this));
+      this.input.on('pointerdown', createCircle, this);
+    } else {
+      this.input.off('pointerdown', createCircle, this);
     }
   }
   
   function createCircle(pointer) {
-    if (isDrawingEnabled) {
-      const x = pointer.x;
-      const y = pointer.y;
-      
-      const circle = this.add.circle(x, y, 5, 0xff0000);  // Usar this.add.circle y reduce el radio
-      circles.add(circle);  // Agregar el círculo al grupo
-      
-      // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
-    }
+    const x = pointer.x;
+    const y = pointer.y;
+  
+    const circle = this.add.circle(x, y, 20, 0xff0000);  // Usar this.add.circle
+    circles.add(circle);  // Agregar el círculo al grupo
+  
+    // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
   }
   
