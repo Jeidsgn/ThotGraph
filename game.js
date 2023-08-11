e29bf4f70cfb7c0015a2a043b0706d98f1c7f8e0
Jeidsgn <jeirm8228@gmail.com>
2023-08-11 16:09:47 -0500

    point

--- game.js
+++ game.js
@@ -4,64 +4,55 @@ const config = {
     height: 600,
     parent: 'game-container',
     scene: {
-      preload: preload,
-      create: create,
-      update: update
+        preload: preload,
+        create: create,
+        update: update
     }
-  };
-  
-  const game = new Phaser.Game(config);
-  
-  let points;  // Para almacenar los puntos dibujados
-  let isDrawingEnabled = false;  // Estado del dibujo
-  let isNextClickForDrawing = false;  // Estado para determinar si el siguiente clic es para dibujar
-  
-  function preload() {
+};
+
+const game = new Phaser.Game(config);
+
+let points;  // Para almacenar los puntos dibujados
+let isDrawingEnabled = false;  // Estado del dibujo
+
+function preload() {
     // Cargar recursos como imágenes y sprites
-  }
-  
-  function create() {
+}
+
+function create() {
     points = this.add.group();  // Crear un grupo para almacenar los puntos
-  
+    
     // Agregar un botón para activar/desactivar el dibujo
     const toggleButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
-      .setInteractive()
-      .on('pointerdown', toggleDrawing.bind(this));
-  
+        .setInteractive()
+        .on('pointerdown', toggleDrawing.bind(this));
+    
     // Configurar la función de clic en el contenedor
-    this.input.on('pointerdown', handlePointerDown.bind(this));
-  }
-  
-  function update() {
+    this.input.on('pointerdown', createPoint.bind(this));
+    this.input.enabled = false;  // Deshabilitar el input al principio
+}
+
+function update() {
     // Actualizaciones del juego en cada fotograma
-  }
-  
-  function toggleDrawing() {
+}
+
+function toggleDrawing() {
     isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
-  
+    
     // Cambiar el texto del botón según el estado del dibujo
     this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
-  
-    // Si el dibujo está activado, habilitar el siguiente clic para dibujar
+    
+    this.input.enabled = isDrawingEnabled;  // Habilitar o deshabilitar el input
+}
+
+function createPoint(pointer) {
     if (isDrawingEnabled) {
-      isNextClickForDrawing = true;
-    } else {
-      isNextClickForDrawing = false;
+        const x = pointer.x;
+        const y = pointer.y;
+        
+        const point = this.add.circle(x, y, 3, 0xff0000);  // Usar this.add.circle
+        points.add(point);  // Agregar el punto al grupo
+        
+        // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el punto
     }
-  }
-  
-  function handlePointerDown(pointer) {
-    if (isNextClickForDrawing) {
-      const x = pointer.x;
-      const y = pointer.y;
-  
-      const point = this.add.circle(x, y, 2, 0xff0000);  // Usar this.add.circle
-      points.add(point);  // Agregar el punto al grupo
-  
-      // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el punto
-  
-      // Restablecer el estado para el siguiente clic
-      isNextClickForDrawing = false;
-    }
-  }
-  
+}
