bf838668c732bcd95d39cbb951b58d5162c66627
Jeidsgn <jeirm8228@gmail.com>
2023-08-10 22:48:43 -0500

    point

--- elements/point.js
+++ elements/point.js
@@ -8,19 +8,11 @@ class Point {
       this.graphics.fillStyle(0xff0000, 1);
       this.graphics.fillCircle(this.x, this.y, 5);
     }
-  
-    // Aquí puedes agregar métodos para la interacción y lógica específica del punto
   }
-
-  let points; // Para almacenar los puntos dibujados
 
   function createPoint(pointer) {
     const x = pointer.x;
     const y = pointer.y;
     
     const point = new Point(this, x, y); // Crear un nuevo punto
-    points = this.add.group(); // Crear un grupo para almacenar los puntos
-    points.add(point); // Agregar el punto al grupo
-  
-    // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el punto
   }  
