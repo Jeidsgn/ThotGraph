500b6b2c22df2be796a26466781064d91d06a10e
Jeidsgn <jeirm8228@gmail.com>
2023-08-11 15:47:26 -0500

    point

--- game.js
+++ game.js
@@ -12,76 +12,46 @@ const config = {
   
   const game = new Phaser.Game(config);
   
-  let puntos;  // Para almacenar los puntos dibujados
-  let isDrawingEnabled = false;  // Estado del dibujo
-  let isMovingEnabled = false;   // Estado del movimiento
+  let circles;
+  let isDrawingEnabled = false;
   
   function preload() {
     // Cargar recursos como imágenes y sprites
   }
   
   function create() {
-    puntos = this.add.group();  // Crear un grupo para almacenar los puntos
-    
-    // Agregar un botón para activar/desactivar el dibujo
-    const toggleDrawingButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
+    circles = this.add.group();
+  
+    const toggleButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
       .setInteractive()
       .on('pointerdown', toggleDrawing.bind(this));
-    
-    // Agregar un botón para activar/desactivar el movimiento
-    const toggleMovingButton = this.add.text(120, 10, 'Activar Mover', { fill: '#ffffff' })
-      .setInteractive()
-      .on('pointerdown', toggleMoving.bind(this));
+  
+    // No configurar el evento de clic aquí
   }
   
   function update() {
-    if (isMovingEnabled) {
-      // Iterar sobre cada punto y actualizar su posición si está siendo arrastrado
-      puntos.getChildren().forEach(point => {
-        if (point.input.draggable && point.input.isDragged) {
-          point.x = game.input.activePointer.x;
-          point.y = game.input.activePointer.y;
-        }
-      });
-    }
+    // Actualizaciones del juego en cada fotograma
   }
   
   function toggleDrawing() {
-    isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
-    isMovingEnabled = false;  // Deshabilitar el movimiento al cambiar el estado de dibujo
-    
-    // Cambiar el texto del botón según el estado del dibujo
+    isDrawingEnabled = !isDrawingEnabled;
+  
     this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
-    // Cambiar el texto del botón de mover según el estado de movimiento
-    this.children.list[1].setText(isMovingEnabled ? 'Desactivar Mover' : 'Activar Mover');
-    
+  
     if (isDrawingEnabled) {
-      this.input.on('pointerdown', createPoint.bind(this));
+      // Configurar el evento de clic aquí, después de cambiar el estado del dibujo
+      this.input.on('pointerdown', createCircle.bind(this));
     } else {
-      this.input.off('pointerdown', createPoint.bind(this));
+      // Eliminar el evento de clic si el dibujo está desactivado
+      this.input.off('pointerdown', createCircle.bind(this));
     }
   }
   
-  function toggleMoving() {
-    isMovingEnabled = !isMovingEnabled;  // Cambiar el estado del movimiento
-    isDrawingEnabled = false;  // Deshabilitar el dibujo al cambiar el estado de movimiento
-    
-    // Cambiar el texto del botón de mover según el estado de movimiento
-    this.children.list[1].setText(isMovingEnabled ? 'Desactivar Mover' : 'Activar Mover');
-    // Cambiar el texto del botón de dibujo según el estado de dibujo
-    this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
-  }
+  function createCircle(pointer) {
+    const x = pointer.x;
+    const y = pointer.y;
   
-  function createPoint(pointer) {
-    if (isDrawingEnabled) {
-      const x = pointer.x;
-      const y = pointer.y;
-  
-      const point = this.add.circle(x, y, 10, 0xff0000);  // Usar this.add.circle
-      puntos.add(point);  // Agregar el punto al grupo
-      point.setInteractive({ draggable: isMovingEnabled });  // Hacer el punto interactivo para arrastrar si el movimiento está habilitado
-  
-      // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el punto
-    }
+    const circle = this.add.circle(x, y, 20, 0xff0000);
+    circles.add(circle);
   }
   
