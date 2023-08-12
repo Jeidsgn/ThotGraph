eab557680439da577d1b5038b08d88dcad499927
Jeidsgn <jeirm8228@gmail.com>
2023-08-12 02:59:00 -0500

    point

--- game.js
+++ game.js
@@ -51,7 +51,7 @@ const config = {
   function handlePointerDown(pointer) {
     if (isDrawingEnabled && waitingForClick) {
       createCircle.call(this, pointer);
-      waitingForClick = false;
+      // waitingForClick = false;
     } else {
       waitingForClick = true;
     }
