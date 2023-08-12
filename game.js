6e5283c8787253d8cc06b6e43d5e5519a0365943
Jeidsgn <jeirm8228@gmail.com>
2023-08-12 01:59:39 -0500

    point

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ const config = {
   
   let circles;  // Para almacenar los círculos dibujados
   let isDrawingEnabled = false;  // Estado del dibujo
-  let lastPointerPosition; // Última posición de clic cuando se desactivó el dibujo
+  let isPendingDrawing = false;  // Variable de espera para el próximo dibujo
   
   function preload() {
     // Cargar recursos como imágenes y sprites
@@ -22,11 +22,14 @@ const config = {
   
   function create() {
     circles = this.add.group();  // Crear un grupo para almacenar los círculos
-      
+    
     // Agregar un botón para activar/desactivar el dibujo
     const toggleButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
       .setInteractive()
       .on('pointerdown', toggleDrawing.bind(this));
+  
+    // Configurar la función de clic en el contenedor
+    this.input.on('pointerdown', handlePointerDown.bind(this));
   }
   
   function update() {
@@ -40,39 +43,31 @@ const config = {
   
   function toggleDrawing() {
     isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
-  
+    
     // Cambiar el texto del botón según el estado del dibujo
     this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
+  }
   
+  function handlePointerDown(pointer) {
     if (isDrawingEnabled) {
-      this.input.on('pointerdown', createCircle.bind(this));
-    } else {
-      lastPointerPosition = null; // Reiniciar la última posición al desactivar el dibujo
-      this.input.off('pointerdown', createCircle.bind(this));
+      if (isPendingDrawing) {
+        createCircle.call(this, pointer);
+        isPendingDrawing = false;
+      } else {
+        isPendingDrawing = true;
+      }
     }
   }
   
   function createCircle(pointer) {
     if (isDrawingEnabled) {
-      if (lastPointerPosition) {
-        // Calcular la distancia entre la última posición y la posición actual
-        const distance = Phaser.Math.Distance.Between(
-          lastPointerPosition.x,
-          lastPointerPosition.y,
-          pointer.x,
-          pointer.y
-        );
+      const x = pointer.x;
+      const y = pointer.y;
   
-        if (distance > 10) { // Solo dibujar si la distancia es mayor a un umbral
-          const circle = this.add.circle(pointer.x, pointer.y, 20, 0xff0000);  // Usar this.add.circle
-          circles.add(circle);  // Agregar el círculo al grupo
-          lastPointerPosition = { x: pointer.x, y: pointer.y }; // Actualizar la última posición
-        }
-      } else {
-        const circle = this.add.circle(pointer.x, pointer.y, 20, 0xff0000);
-        circles.add(circle);
-        lastPointerPosition = { x: pointer.x, y: pointer.y };
-      }
+      const circle = this.add.circle(x, y, 20, 0xff0000);  // Usar this.add.circle
+      circles.add(circle);  // Agregar el círculo al grupo
+  
+      // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
     }
   }
   
