e2d496724f49c5bf97b2e49d58fe0c3807811d0b
Jeidsgn <jeirm8228@gmail.com>
2023-08-12 00:23:53 -0500

    point

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ const config = {
   
   let circles;  // Para almacenar los círculos dibujados
   let isDrawingEnabled = false;  // Estado del dibujo
-  let pointerDownCallback; // Función de callback para el evento pointerdown
+  let waitForClick = false; // Variable de espera para el próximo clic
   
   function preload() {
     // Cargar recursos como imágenes y sprites
@@ -22,21 +22,14 @@ const config = {
   
   function create() {
     circles = this.add.group();  // Crear un grupo para almacenar los círculos
-      
+  
     // Agregar un botón para activar/desactivar el dibujo
     const toggleButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
       .setInteractive()
       .on('pointerdown', toggleDrawing.bind(this));
   
     // Configurar la función de clic en el contenedor solo si el dibujo está activado
-    pointerDownCallback = function(pointer) {
-      if (isDrawingEnabled) {
-        createCircle.call(this, pointer);
-      }
-    };
-  
-    // Configurar el evento pointerdown con la función de callback
-    this.input.on('pointerdown', pointerDownCallback);
+    this.input.on('pointerdown', handlePointerDown.bind(this));
   }
   
   function update() {
@@ -50,18 +43,28 @@ const config = {
   
   function toggleDrawing() {
     isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
+    waitForClick = !isDrawingEnabled; // Actualizar la variable de espera
   
     // Cambiar el texto del botón según el estado del dibujo
     this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
   }
   
+  function handlePointerDown(pointer) {
+    if (waitForClick) {
+      waitForClick = false; // Se ha hecho clic después de activar el dibujo
+      createCircle.call(this, pointer);
+    }
+  }
+  
   function createCircle(pointer) {
-    const x = pointer.x;
-    const y = pointer.y;
+    if (isDrawingEnabled) {
+      const x = pointer.x;
+      const y = pointer.y;
   
-    const circle = this.add.circle(x, y, 20, 0xff0000);  // Usar this.add.circle
-    circles.add(circle);  // Agregar el círculo al grupo
+      const circle = this.add.circle(x, y, 20, 0xff0000);  // Usar this.add.circle
+      circles.add(circle);  // Agregar el círculo al grupo
   
-    // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
+      // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
+    }
   }
   
