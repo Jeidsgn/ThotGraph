74913a4451a346843702826ceee9b59be31e39ee
Jeidsgn <jeirm8228@gmail.com>
2023-08-12 00:02:17 -0500

    point

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ const config = {
   
   let circles;  // Para almacenar los círculos dibujados
   let isDrawingEnabled = false;  // Estado del dibujo
+  let isWaitingForFirstClick = false;  // Estado para esperar el primer clic después de la activación
   
   function preload() {
     // Cargar recursos como imágenes y sprites
@@ -21,49 +22,45 @@ const config = {
   
   function create() {
     circles = this.add.group();  // Crear un grupo para almacenar los círculos
-    
+      
     // Agregar un botón para activar/desactivar el dibujo
     const toggleButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
       .setInteractive()
       .on('pointerdown', toggleDrawing.bind(this));
-  
-    // Configurar la función de clic en el contenedor
-    this.input.on('pointerdown', createCircle.bind(this));
-}
-
+  }
   
   function update() {
-  if (isDrawingEnabled) {
-    circles.children.iterate(circle => {
-      // Aplica aquí las modificaciones o actualizaciones que necesitas en cada círculo
-      // por ejemplo: circle.x += 1; para mover el círculo hacia la derecha
-    });
+    if (isWaitingForFirstClick && isDrawingEnabled) {
+      this.input.on('pointerdown', createCircle.bind(this));
+      isWaitingForFirstClick = false;
+    }
+  
+    if (isDrawingEnabled) {
+      circles.children.iterate(circle => {
+        // Aplica aquí las modificaciones o actualizaciones que necesitas en cada círculo
+        // por ejemplo: circle.x += 1; para mover el círculo hacia la derecha
+      });
+    }
   }
-}
   
   function toggleDrawing() {
     isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
   
+    if (isDrawingEnabled) {
+      isWaitingForFirstClick = true; // Esperar el primer clic después de la activación
+    }
+  
     // Cambiar el texto del botón según el estado del dibujo
     this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
-  
-    if (isDrawingEnabled) {
-      this.input.on('pointerdown', createCircle.bind(this));
-    } else {
-      this.input.off('pointerdown', createCircle.bind(this));
-    }
   }
   
   function createCircle(pointer) {
-    if (isDrawingEnabled) {
-        const x = pointer.x;
-        const y = pointer.y;
-      
-        const circle = this.add.circle(x, y, 20, 0xff0000);  // Usar this.add.circle
-        circles.add(circle);  // Agregar el círculo al grupo
-      
-        // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
-    }
-}
-
+    const x = pointer.x;
+    const y = pointer.y;
+  
+    const circle = this.add.circle(x, y, 20, 0xff0000);  // Usar this.add.circle
+    circles.add(circle);  // Agregar el círculo al grupo
+  
+    // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
+  }
   
