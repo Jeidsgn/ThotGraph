43b4078c94e95c6f3d901d9618f2da692009da74
Jeidsgn <jeirm8228@gmail.com>
2023-08-12 04:34:31 -0500

    point

--- game.js
+++ game.js
@@ -62,15 +62,19 @@ const config = {
   }
   
   function createPoint(pointer) {
-    const x = pointer.x;
-    const y = pointer.y;
+    if (pointer) {
+      const x = pointer.x || 0;  // Si pointer.x no está definido, usa 0 como valor predeterminado
+      const y = pointer.y || 0;  // Si pointer.y no está definido, usa 0 como valor predeterminado
   
-    const point = this.add.graphics();
-    point.fillStyle(0xff0000);  // Color del punto
-    point.fillCircle(x, y, 5);  // Dibujar un punto con radio de 5 píxeles
-    points.add(point);  // Agregar el punto al grupo
+      const point = this.add.graphics();
+      point.fillStyle(0xff0000);  // Color del punto
+      point.fillCircle(x, y, 5);  // Dibujar un punto con radio de 5 píxeles
+      points.add(point);  // Agregar el punto al grupo
   
-    const letter = String.fromCharCode(65 + points.getLength() - 1); // Convertir número en letra (A, B, C, ...)
-    textContainer.text += letter + ' '; // Agregar la letra al contenedor de texto
+      const letter = String.fromCharCode(65 + points.getLength() - 1); // Convertir número en letra (A, B, C, ...)
+      textContainer.text += letter + ' '; // Agregar la letra al contenedor de texto
+    }
   }
+  
+
   
