945fa49d3ccf00104999bfd049c78b0853174ff0
Jeidsgn <jeirm8228@gmail.com>
2023-08-12 03:32:34 -0500

    point

--- game.js
+++ game.js
@@ -8,61 +8,55 @@ const config = {
       create: create,
       update: update
     }
-  };
-  
-  const game = new Phaser.Game(config);
-  
-  let circles;  // Para almacenar los círculos dibujados
-  let isDrawingEnabled = false;  // Estado del dibujo
-  let waitingForClick = true;  // Variable de espera
-  
-  function preload() {
+};
+
+const game = new Phaser.Game(config);
+
+let points;  // Para almacenar los puntos dibujados
+let isDrawingEnabled = false;  // Estado del dibujo
+let waitingForClick = true;  // Variable de espera
+
+function preload() {
     // Cargar recursos como imágenes y sprites
-  }
-  
-  function create() {
-    circles = this.add.group();  // Crear un grupo para almacenar los círculos
-  
+}
+
+function create() {
+    points = this.add.group();  // Crear un grupo para almacenar los puntos
+
     // Agregar un botón para activar/desactivar el dibujo
     const toggleButton = this.add.text(10, 10, 'Activar Dibujo', { fill: '#ffffff' })
-      .setInteractive()
-      .on('pointerdown', toggleDrawing.bind(this));
-  
+        .setInteractive()
+        .on('pointerdown', toggleDrawing.bind(this));
+
     // Configurar la función de clic en el contenedor
     this.input.on('pointerdown', handlePointerDown.bind(this));
-  }
-  
-  function update() {
+}
+
+function update() {
     if (isDrawingEnabled) {
-      circles.children.iterate(circle => {
-        // Aplica aquí las modificaciones o actualizaciones que necesitas en cada círculo
-        // por ejemplo: circle.x += 1; para mover el círculo hacia la derecha
-      });
+        if (!waitingForClick) {
+            const pointer = this.input.activePointer;
+            createPoint.call(this, pointer.x, pointer.y);  // Crear punto cuando se mueve el mouse
+        }
     }
-  }
-  
-  function toggleDrawing() {
+}
+
+function toggleDrawing() {
     isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
     waitingForClick = true;  // Restablecer waitingForClick a true al desactivar el dibujo
+    
     // Cambiar el texto del botón según el estado del dibujo
     this.children.list[0].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
-  }
-  
-  function handlePointerDown(pointer) {
+}
+
+function handlePointerDown(pointer) {
     if (isDrawingEnabled && waitingForClick) {
-      waitingForClick = false;  // Cambiar a false después del primer clic
-    } else if (isDrawingEnabled && !waitingForClick) {
-      createCircle.call(this, pointer);  // Crear el círculo sin esperar después del primer clic
+        waitingForClick = false;  // Cambiar a false después del primer clic
+        createPoint.call(this, pointer.x, pointer.y);  // Crear punto en el primer clic
     }
-  }
-  
-  function createCircle(pointer) {
-    const x = pointer.x;
-    const y = pointer.y;
-  
-    const circle = this.add.circle(x, y, 20, 0xff0000);  // Usar this.add.circle
-    circles.add(circle);  // Agregar el círculo al grupo
-  
-    // Aquí puedes realizar las verificaciones y lógica adicional para dibujar el círculo
-  }
-  
+}
+
+function createPoint(x, y) {
+    const point = this.add.circle(x, y, 2, 0xff0000);  // Usar un círculo pequeño como punto
+    points.add(point);  // Agregar el punto al grupo
+}
