770e5900e5361dd67eb0af5084f87e434a7b1fa6
Jeidsgn <jeirm8228@gmail.com>
2023-08-12 13:14:23 -0500

    point

--- game.js
+++ game.js
@@ -12,70 +12,72 @@ const config = {
   
   const game = new Phaser.Game(config);
   
-  let points;  // Para almacenar los puntos dibujados
-  let isDrawingEnabled = false;  // Estado del dibujo
-  let textContainer; // Contenedor para mostrar letras
-  let waitingForClick = true;  // Variable de espera
+  let points;
+  let isDrawingEnabled = false;
+  let textContainer;
+  let waitingForClick = true;
   
   function preload() {
     // Cargar recursos como imágenes y sprites
   }
   
   function create() {
-    points = this.add.group();  // Crear un grupo para almacenar los puntos
-    textContainer = this.add.text(10, 10, '', { fill: '#ffffff' }); // Crear el contenedor de texto
-    
-    // Agregar un botón para activar/desactivar el dibujo
+    points = this.add.group();
+    textContainer = this.add.text(10, 10, '', { fill: '#ffffff' });
+  
     const toggleButton = this.add.text(10, 550, 'Activar Dibujo', { fill: '#ffffff' })
       .setInteractive()
       .on('pointerdown', toggleDrawing.bind(this));
-      
-    // Configurar la función de clic en el contenedor
+  
     this.input.on('pointerdown', handlePointerDown.bind(this));
   }
   
   function update() {
-    
-  if (isDrawingEnabled) {
-    if (!waitingForClick) {
-      createPoint.call(this);  // Crear un punto si el dibujo está habilitado y no se espera clic
-      points.children.iterate(point => {
-        // Aplica aquí las modificaciones o actualizaciones que necesitas en cada punto
-        // por ejemplo: point.x += 1; para mover el punto hacia la derecha
-      });
+    if (isDrawingEnabled) {
+      if (!waitingForClick) {
+        points.children.iterate(point => {
+          // Aplica aquí las modificaciones o actualizaciones que necesitas en cada punto
+          // por ejemplo: point.x += 1; para mover el punto hacia la derecha
+        });
+      }
     }
-  }
   }
   
   function toggleDrawing() {
-    isDrawingEnabled = !isDrawingEnabled;  // Cambiar el estado del dibujo
-    waitingForClick = true;  // Cambiar a false después del primer clic
-    // Cambiar el texto del botón según el estado del dibujo
+    isDrawingEnabled = !isDrawingEnabled;
+    waitingForClick = true;
     this.children.list[1].setText(isDrawingEnabled ? 'Desactivar Dibujo' : 'Activar Dibujo');
   }
   
   function handlePointerDown(pointer) {
     if (isDrawingEnabled && waitingForClick) {
-      waitingForClick = false;  // Cambiar a false después del primer clic
+      waitingForClick = false;
     } else if (isDrawingEnabled && !waitingForClick) {
-      createPoint.call(this, pointer);  // Crear el círculo sin esperar después del primer clic
+      createPoint.call(this, pointer);
     }
   }
   
   function createPoint(pointer) {
     if (pointer) {
-      const x = pointer.x || 0;  // Si pointer.x no está definido, usa 0 como valor predeterminado
-      const y = pointer.y || 0;  // Si pointer.y no está definido, usa 0 como valor predeterminado
+      const x = pointer.x || 0;
+      const y = pointer.y || 0;
   
       const point = this.add.graphics();
-      point.fillStyle(0xff0000);  // Color del punto
-      point.fillCircle(x, y, 5);  // Dibujar un punto con radio de 5 píxeles
-      points.add(point);  // Agregar el punto al grupo
+      point.fillStyle(0xff0000);
+      point.fillCircle(x, y, 10);
+      points.add(point);
   
-      const letter = String.fromCharCode(65 + points.getLength() - 1); // Convertir número en letra (A, B, C, ...)
-      textContainer.text += letter + ' '; // Agregar la letra al contenedor de texto
+      point.setInteractive({ draggable: true });
+      point.on('dragstart', function(pointer) {
+        isDrawingEnabled = false;
+      });
+      point.on('drag', function(pointer, dragX, dragY) {
+        this.x = dragX;
+        this.y = dragY;
+      });
+  
+      const letter = String.fromCharCode(65 + points.getLength() - 1);
+      textContainer.text += letter + ' ';
     }
   }
-  
-
   
