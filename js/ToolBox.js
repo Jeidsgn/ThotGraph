ae82e56dc117bd8c0dad322e266d5001ece6f992
Jeidsgn <jeirm8228@gmail.com>
2023-08-13 21:06:18 -0500

    structure

--- js/ToolBox.js
+++ js/ToolBox.js
@@ -1,55 +1,58 @@
 export class ToolBox {
-    constructor(scene) {
-      this.scene = scene;
-      this.scene.toolboxButtons = []; // Initialize the array here
-      this.scene.activeButton = null; // Referencia al botón activo
-      this.scene.elementNames = [];// Array para almacenar los nombres de los elementos.
-    }
+  constructor(scene) {
+    this.scene = scene;
+    this.scene.toolboxButtons = []; // Initialize the array here
+    this.scene.activeButton = null; // Referencia al botón activo
+    this.scene.elementNames = []; // Array para almacenar los nombres de los elementos.
+  }
 
-    createToolbox() {
-      // Crear botones base y botones dependientes (según elementos en elements/)
-      
-      this.createBaseButtons();
-      this.createDependentButtons();
-    }
-  
-    createBaseButtons() {
-      // Crear botones base y agregarlos al toolbox
-      const moveButton = this.scene.add
-        .text(10, 550, "Mover", { fill: "#ffffff" })
-        .setInteractive()
-        .on("pointerdown", () => this.activateButton("Mover"));
-  
-      this.scene.toolboxButtons.push(moveButton); // Use the scene's array
-    }
+  createToolbox() {// Crear botones base y botones dependientes (según elementos en elements/)
+    this.createBaseButtons();
+    this.createDependentButtons();
+  }
 
-createDependentButtons() {
-    for (let i = 0; i < this.scene.elementNames.length; i++) { // Use this.scene.elementNames.length
+  createBaseButtons() {// Crear botones base y agregarlos al toolbox
+    const moveButton = this.scene.add
+      .text(10, 550, "Mover", { fill: "#ffffff" })
+      .setInteractive()
+      .on("pointerdown", () => this.activateButton("Mover"));
+
+    this.scene.toolboxButtons.push(moveButton); // Use the scene's array
+  }
+
+  createDependentButtons() {
+    for (let i = 0; i < this.scene.elementNames.length; i++) {
+      // Use this.scene.elementNames.length
       const button = this.scene.add
-        .text(100 + i * 100, 550, this.scene.elementNames[i], { fill: "#ffffff" })
+        .text(100 + i * 100, 550, this.scene.elementNames[i], {
+          fill: "#ffffff",
+        })
         .setInteractive()
-        .on("pointerdown", () => this.activateButton(this.scene.elementNames[i])); // Use this.scene.elementNames[i]
-  
+        .on("pointerdown", () =>
+          this.activateButton(this.scene.elementNames[i])
+        ); // Use this.scene.elementNames[i]
+
       this.scene.toolboxButtons.push(button);
     }
   }
-  
+
   activateButton(buttonName) {
-    if (this.scene.activeButton) { // Use this.scene.activeButton
-      this.scene.activeButton.setStyle({ fill: "#ffffff" }); // Use this.scene.activeButton
+    if (this.scene.activeButton) {
+      this.scene.activeButton.setStyle({ fill: "#ffffff" });
     }
   
     this.scene.activeButton = this.scene.toolboxButtons.find(button => button.text === buttonName);
     if (this.scene.activeButton) {
       this.scene.activeButton.setStyle({ fill: "#00ff00" });
-      this.scene.isDrawingEnabled = !this.scene.isDrawingEnabled; // Use this.scene.isDrawingEnabled
+      this.scene.isDrawingEnabled = !this.scene.isDrawingEnabled;
       this.scene.waitingForClick = true;
   
-      this.scene.children.list[1].setText(
-        this.scene.isDrawingEnabled ? "Desactivar Dibujo" : "Activar Dibujo"
-      );
+      const textContainer = this.scene.children.list.find(child => child instanceof Phaser.GameObjects.Text); // Find the text container
+      if (textContainer) {
+        textContainer.setText(
+          this.scene.isDrawingEnabled ? "Desactivar Dibujo" : "Activar Dibujo"
+        );
+      }
     }
   }
-  
-  }
-  
+}
