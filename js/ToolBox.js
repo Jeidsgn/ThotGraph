e0bc3086028798faec58b0e1a7463c84b84981f2
Jeidsgn <jeirm8228@gmail.com>
2023-08-13 20:48:46 -0500

    structure

--- js/ToolBox.js
+++ js/ToolBox.js
@@ -3,11 +3,12 @@ export class ToolBox {
       this.scene = scene;
       this.scene.toolboxButtons = []; // Initialize the array here
       this.scene.activeButton = null; // Referencia al botón activo
+      this.scene.elementNames = [];// Array para almacenar los nombres de los elementos.
     }
 
     createToolbox() {
       // Crear botones base y botones dependientes (según elementos en elements/)
-      this.elementNames = [];// Array para almacenar los nombres de los elementos.
+      
       this.createBaseButtons();
       this.createDependentButtons();
     }
