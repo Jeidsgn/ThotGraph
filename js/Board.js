ff32c6fef5127157217072dc1ee48063d71e5940
Jeidsgn <jeirm8228@gmail.com>
2023-08-14 00:24:35 -0500

    structure

--- js/Board.js
+++ js/Board.js
@@ -17,7 +17,6 @@ export class Board extends Phaser.Scene {
     this.elements = new Element(this);
     // Inicializa los nombres y elementos
     this.elements.Names();
-    this.elements.init();
   }
 
   // Precarga de recursos (imágenes, sprites, etc.)
