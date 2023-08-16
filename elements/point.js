904518a35f31bd36d43b3c97dc9c9f4611ee68e9
Jeidsgn <jeirm8228@gmail.com>
2023-08-16 00:52:40 -0500

    move

--- elements/point.js
+++ elements/point.js
@@ -4,11 +4,12 @@ export class Point {
       this.points = scene.add.group(); // Grupo para almacenar los puntos en la escena
       this.textContainer = scene.add.text(10, 10, "", { fill: "#ffffff" }); // Contenedor de texto para las letras de los puntos
       this.isClicking = false; // Variable para controlar si se está haciendo clic
+      this.elementalpointer = { x: 0, y: 0 }; // Almacena la posición del puntero
   
       // Configura el evento de clic en la escena para capturar el puntero
       this.scene.input.on("pointerdown", (pointer) => {
         this.isClicking = true; // Se está haciendo clic
-        this.elementalpointer = pointer;
+        this.elementalpointer = { x: pointer.x, y: pointer.y }; // Almacena la posición del puntero
       });
   
       // Configura el evento de liberación del clic para controlar cuando se deja de hacer clic
@@ -22,23 +23,26 @@ export class Point {
     }
   
     createPoint() {
-        if (this.isClicking) {
-      const x = this.elementalpointer.x || 0;
-      const y = this.elementalpointer.y || 0;
-      const point = this.scene.add.graphics();
-      point.fillStyle(0xff0000);
-      point.fillCircle(x, y, 5);
-      this.points.add(point); // Añade el punto al grupo
+      if (this.isClicking) {
+        const x = this.elementalpointer.x;
+        const y = this.elementalpointer.y;
+        const point = this.scene.add.graphics();
+        point.fillStyle(0xff0000);
+        point.fillCircle(x, y, 5);
+        this.points.add(point); // Añade el punto al grupo
   
-      const letter = String.fromCharCode(65 + this.points.getLength() - 1);
-      this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
-        } else {
-            return;
-        }
+        const letter = String.fromCharCode(65 + this.points.getLength() - 1);
+        this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
+        this.isClicking = false; // Desactiva el clic para evitar creación continua en el mismo clic
+      }
     }
   
-    movePoint() {
-      // Implementa la lógica para mover los puntos interactivamente
+    movePoint(index, x, y) {
+      if (index >= 0 && index < this.points.getLength()) {
+        const point = this.points.getChildren()[index];
+        point.x = x;
+        point.y = y;
+      }
     }
   }
   
