d1850698b576d35cd19f8be2fdc43fd50ea1ab8e
Jeidsgn <jeirm8228@gmail.com>
2023-08-16 02:37:07 -0500

    move

--- elements/point.js
+++ elements/point.js
@@ -1,28 +1,25 @@
 export class Point {
     constructor(scene) {
       this.scene = scene;
-      this.points = scene.add.group();
-      this.textContainer = scene.add.text(10, 10, "", { fill: "#ffffff" });
-      this.isClicking = false;
-      this.elementalpointer = { x: 0, y: 0 };
-      this.interactivePoints = [];
+      this.points = scene.add.group(); // Grupo para almacenar los puntos en la escena
+      this.textContainer = scene.add.text(10, 10, "", { fill: "#ffffff" }); // Contenedor de texto para las letras de los puntos
+      this.isClicking = false; // Variable para controlar si se está haciendo clic
+      this.elementalpointer = { x: 0, y: 0 }; // Almacena la posición del puntero
   
+      // Configura el evento de clic en la escena para capturar el puntero
       this.scene.input.on("pointerdown", (pointer) => {
-        this.isClicking = true;
-        this.elementalpointer = { x: pointer.x, y: pointer.y };
+        this.isClicking = true; // Se está haciendo clic
+        this.elementalpointer = { x: pointer.x, y: pointer.y }; // Almacena la posición del puntero
       });
   
+      // Configura el evento de liberación del clic para controlar cuando se deja de hacer clic
       this.scene.input.on("pointerup", () => {
-        this.isClicking = false;
-      });
-  
-      this.scene.input.on("pointermove", (pointer) => {
-        this.movePoint(pointer.x, pointer.y);
+        this.isClicking = false; // No se está haciendo clic
       });
     }
   
     addName() {
-      this.scene.elementNames.push("Point");
+      this.scene.elementNames.push("Point"); // Agrega el nombre "Point" al array de nombres de elementos en la escena
     }
   
     createPoint() {
@@ -32,41 +29,15 @@ export class Point {
         const point = this.scene.add.graphics();
         point.fillStyle(0xff0000);
         point.fillCircle(x, y, 5);
-        this.points.add(point);
+        this.points.add(point); // Añade el punto al grupo
   
         const letter = String.fromCharCode(65 + this.points.getLength() - 1);
-        this.textContainer.text += letter + " ";
-  
-        this.interactivePoints.push({
-          circle: point,
-          isHovered: false
-        });
-  
-        this.isClicking = false;
+        this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
+        this.isClicking = false; // Desactiva el clic para evitar creación continua en el mismo clic
       }
     }
   
-    movePoint(x, y) {
-      for (const interactivePoint of this.interactivePoints) {
-        const circle = interactivePoint.circle;
-        const distance = Phaser.Math.Distance.Between(x, y, circle.x, circle.y);
-  
-        if (distance <= 5) {
-          if (!interactivePoint.isHovered) {
-            interactivePoint.isHovered = true;
-            circle.clear();
-            circle.fillStyle(0x00ff00);
-            circle.fillCircle(circle.x, circle.y, 5);
-          }
-        } else {
-          if (interactivePoint.isHovered) {
-            interactivePoint.isHovered = false;
-            circle.clear();
-            circle.fillStyle(0xff0000);
-            circle.fillCircle(circle.x, circle.y, 5);
-          }
-        }
-      }
+    movePoint(x, y) { //aquí va la lógica del movimiento
+      console.log("movepoint") //verificación
     }
   }
-  
