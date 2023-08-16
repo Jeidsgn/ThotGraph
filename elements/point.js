5cd7b7094e456c447593ed95e5496dbd56ba45a6
Jeidsgn <jeirm8228@gmail.com>
2023-08-15 23:47:04 -0500

    move

--- elements/point.js
+++ elements/point.js
@@ -4,23 +4,45 @@ export class Point {
       this.points = scene.add.group(); // Grupo para almacenar los puntos en la escena
       this.textContainer = scene.add.text(10, 10, "", { fill: "#ffffff" }); // Contenedor de texto para las letras de los puntos
       this.selectedPoint = null; // Punto seleccionado para mover
-      this.elementalpointer = null; // Puntero capturado al hacer clic
-      
+      this.movepoint = false; // Variable para habilitar el modo de movimiento
+      this.elementalpointer = null; // Puntero actual
+      this.setupEventListeners();
+    }
+  
+    setupEventListeners() {
       // Configura el evento de clic en la escena para capturar el puntero
       this.scene.input.on("pointerdown", (pointer) => {
         this.elementalpointer = pointer;
-        this.checkPointSelection(pointer);
+        if (!this.movepoint) {
+          this.createPoint();
+        } else {
+          this.selectPointToMove();
+        }
       });
-      
-      // Configura el evento de movimiento del puntero para mover el punto seleccionado
-      this.scene.input.on("pointermove", (pointer) => {
-        this.movePoint(pointer);
+  
+      // Configura evento para cambiar el color del punto al pasar el cursor por encima
+      this.scene.input.on("pointerover", (pointer) => {
+        if (!this.movepoint) return;
+        const overPoint = this.getPointAtPosition(pointer.x, pointer.y);
+        if (overPoint) {
+          overPoint.setFillStyle(0x00ff00);
+        }
       });
-      
-      // Configura el evento de liberación del puntero para dejar de mover el punto
-      this.scene.input.on("pointerup", () => {
-        this.selectedPoint = null;
-        this.elementalpointer = "null";
+  
+      // Configura evento para restaurar el color original del punto al salir del cursor
+      this.scene.input.on("pointerout", (pointer) => {
+        if (!this.movepoint) return;
+        const overPoint = this.getPointAtPosition(pointer.x, pointer.y);
+        if (overPoint) {
+          overPoint.setFillStyle(0xff0000);
+        }
+      });
+    }
+  
+    getPointAtPosition(x, y) {
+      return this.points.getChildren().find((point) => {
+        const bounds = point.getBounds();
+        return bounds.contains(x, y);
       });
     }
   
@@ -29,35 +51,34 @@ export class Point {
     }
   
     createPoint() {
-      if (this.elementalpointer == "null") {
-        const x = this.elementalpointer.x || 0;
-        const y = this.elementalpointer.y || 0;
-        const point = this.scene.add.graphics();
-        point.fillStyle(0xff0000);
-        point.fillCircle(x, y, 5);
-        point.setInteractive({ useHandCursor: true }); // Hace que el punto sea interactivo y muestre el cursor de mano
-        point.on("pointerover", () => point.setFillStyle(0xffff00)); // Cambia el color al pasar el cursor
-        point.on("pointerout", () => point.setFillStyle(0xff0000)); // Cambia el color al salir el cursor
-        point.on("pointerdown", () => this.selectedPoint = point); // Selecciona el punto al hacer clic
-        this.points.add(point); // Añade el punto al grupo
+      if (!this.elementalpointer) return;
   
-        const letter = String.fromCharCode(65 + this.points.getLength() - 1);
-        this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
+      const x = this.elementalpointer.x || 0;
+      const y = this.elementalpointer.y || 0;
+      const point = this.scene.add.graphics();
+      point.fillStyle(0xff0000);
+      point.fillCircle(x, y, 5);
+      this.points.add(point); // Añade el punto al grupo
+  
+      const letter = String.fromCharCode(65 + this.points.getLength() - 1);
+      this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
+    }
+  
+    selectPointToMove() {
+      const selectedPoint = this.getPointAtPosition(
+        this.elementalpointer.x,
+        this.elementalpointer.y
+      );
+      if (selectedPoint) {
+        this.selectedPoint = selectedPoint;
+        this.selectedPoint.setFillStyle(0x00ff00);
       }
     }
   
-    movePoint(pointer) {
-      if (this.selectedPoint) {
-        this.selectedPoint.x = pointer.x;
-        this.selectedPoint.y = pointer.y;
-      }
-    }
-    
-    checkPointSelection(pointer) {
-      // Comprueba si el puntero está sobre algún punto y lo selecciona
-      const pointUnderPointer = this.points.getChildren().find((point) => point.getBounds().contains(pointer.x, pointer.y));
-      if (pointUnderPointer) {
-        this.selectedPoint = pointUnderPointer;
+    movePoint() {
+      if (this.selectedPoint && this.elementalpointer) {
+        this.selectedPoint.x = this.elementalpointer.x;
+        this.selectedPoint.y = this.elementalpointer.y;
       }
     }
   }
