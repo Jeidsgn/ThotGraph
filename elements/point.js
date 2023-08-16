a46ecf3e0efdd61d509ab2357586826d16c5fa74
Jeidsgn <jeirm8228@gmail.com>
2023-08-16 00:19:21 -0500

    move

--- elements/point.js
+++ elements/point.js
@@ -1,34 +1,30 @@
 export class Point {
-  constructor(scene) {
-    this.scene = scene;
-    this.points = scene.add.group(); // Grupo para almacenar los puntos en la escena
-    this.textContainer = scene.add.text(10, 10, "", { fill: "#ffffff" }); // Contenedor de texto para las letras de los puntos
-    this.selectedPoint = null; // Punto seleccionado para mover
-    // Configura el evento de clic en la escena para capturar el puntero
-    this.scene.input.on("pointerdown", (pointer) => {
-      this.elementalpointer = pointer;
-    });
-    this.scene.input.on("pointerup", () => {
-        this.elementalpointer = null;
-    });
-  }
-  addName() {
-    this.scene.elementNames.push("Point"); // Agrega el nombre "Point" al array de nombres de elementos en la escena
-  }
-  createPoint() {
-    if(this.elementalpointer){
-      const x = this.elementalpointer.x || 0;
-      const y = this.elementalpointer.y || 0;
-      const point = this.scene.add.graphics();
-      point.fillStyle(0xff0000);
-      point.fillCircle(x, y, 5);
-      this.points.add(point); // Añade el punto al grupo
-
-      const letter = String.fromCharCode(65 + this.points.getLength() - 1);
-      this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
+    constructor(scene) {
+      this.scene = scene;
+      this.points = scene.add.group();
+      this.textContainer = scene.add.text(10, 10, "", { fill: "#ffffff" });
+    }
+  
+    addName() {
+      this.scene.elementNames.push("Point");
+    }
+  
+    createPoint() {
+      this.scene.input.on("pointerdown", (pointer) => {
+        const x = pointer.x || 0;
+        const y = pointer.y || 0;
+        const point = this.scene.add.graphics();
+        point.fillStyle(0xff0000);
+        point.fillCircle(x, y, 5);
+        this.points.add(point);
+  
+        const letter = String.fromCharCode(65 + this.points.getLength() - 1);
+        this.textContainer.text += letter + " ";
+      });
+    }
+  
+    movePoint() {
+      // Implement your point moving logic here
     }
   }
-
-  movePoint() {
-  }
-}
+  
