f74482bd541c8f46f970f07b667a0475ac49592b
Jeidsgn <jeirm8228@gmail.com>
2023-08-16 22:39:40 -0500

    move

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import {Board} from './js/Board.js';
 // Configuración del juego
 const config = {
     type: Phaser.AUTO,
-    width: window.innerWidth * 0.9, // Usar el ancho de la ventana
-    height: window.innerHeight * 0.9, // Usar la altura de la ventana
+    width: window.innerWidth, // Usar el ancho de la ventana
+    height: window.innerHeight, // Usar la altura de la ventana
     parent: 'game-container',   
     scene: [Board],
     add: {
