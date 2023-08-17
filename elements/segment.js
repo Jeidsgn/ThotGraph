2b2cae3216473c8b1ee2da6f523be70548e2b8ae
Jeidsgn <jeirm8228@gmail.com>
2023-08-17 16:13:45 -0500

    create segment

--- elements/segment.js
+++ elements/segment.js
@@ -18,54 +18,35 @@ export class Segment {
         // Configura el evento de liberación del clic para controlar cuando se deja de hacer clic
         this.scene.input.on("pointerup", () => {
             this.isClicking = false; // No se está haciendo clic
+            if (this.pointA && this.pointB) {
+                this.segments.push(new Phaser.Geom.Line(this.pointA.x, this.pointA.y, this.pointB.x, this.pointB.y));
+            }
+            this.pointA = null;
+            this.pointB = null;
         });
     }
     addName() {
         this.scene.elementNames.push("Segment"); // Agrega el nombre al array de nombres de elementos en la escena
     }
-
-    Startpoint(interactivePoint) {
-
-        if (this.PointB == null) {
-            const pt = interactivePoint;
-        } else {
-            const pt = this.pointA;
-        }
-        return pt
-    }
-
-    Startpoint(interactivePoint) {
-
-        if (this.PointB == null) {
-            const pt = interactivePoint;
-        } else {
-            const pt = this.pointA;
-        }
-        return pt
-    }
-
     createSegment() {
-        // Verifica si hay un punto almacenado como punto de inicio del segmento
-        if (this.pointA === null) {
-            // Itera a través de los puntos interactivos en la escena
-            for (const interactivePoint of this.scene.interactivePoints) {
-                // Verifica si el puntero se encuentra dentro del área del punto interactivo
-                if (Phaser.Geom.Rectangle.ContainsPoint(interactivePoint.area, this.scene.input.activePointer)) {
-                    this.pointA = interactivePoint;
-                    break; // Rompe el ciclo al encontrar el primer punto válido
+        // Itera a través de los puntos interactivos en la escena
+        for (const interactivePoint of this.scene.interactivePoints) {
+            if (Phaser.Geom.Rectangle.ContainsPoint(interactivePoint.area, this.scene.input.activePointer)) {
+                // Cambia el aspecto visual del punto interactivo
+                interactivePoint.point.clear();
+                interactivePoint.point.fillStyle(0x00ff00); // Cambia el color a verde
+                interactivePoint.point.fillCircle(
+                    interactivePoint.x,
+                    interactivePoint.y,
+                    5
+                );
+                
+                if (!this.pointA) {
+                    this.pointA = new Phaser.Geom.Point(interactivePoint.x, interactivePoint.y);
+                } else {
+                    this.pointB = new Phaser.Geom.Point(interactivePoint.x, interactivePoint.y);
                 }
             }
-        } else {
-            // Asigna el punto actual del puntero como punto final del segmento
-            this.pointB = new Point(this.scene, this.scene.input.activePointer.x, this.scene.input.activePointer.y);
-            
-            // Crea una nueva instancia de un segmento en base a los puntos A y B
-            const newSegment = new Phaser.Geom.Line(this.pointA.x, this.pointA.y, this.pointB.x, this.pointB.y);
-            this.segments.push(newSegment);
-            
-            // Reinicia los puntos de inicio y final para permitir la creación de otro segmento
-            this.pointA = null;
-            this.pointB = null;
         }
     }
 }
