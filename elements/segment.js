0071ad997dcd6819492a7d8c98e723886468896d
Jeidsgn <jeirm8228@gmail.com>
2023-08-17 19:07:22 -0500

    create segment

--- elements/segment.js
+++ elements/segment.js
@@ -4,49 +4,91 @@ export class Segment {
     constructor(scene) {
         this.scene = scene;
         this.segments = []; // Arreglo para almacenar los segmentos
-        this.pointA = null;
+        this.point = new Point(scene);
+        //this.scene.interactivePoints
         this.pointB = null;
+        this.pointA = null;
 
         this.isClicking = false; // Variable para controlar si se está haciendo clic
+        this.pointermove = { x: 0, y: 0 }; // Almacena la posición del puntero
 
         // Configura el evento de clic en la escena para capturar el puntero
         this.scene.input.on("pointerdown", (pointer) => {
             this.isClicking = true; // Se está haciendo clic
-            this.createSegment(); // Intenta crear el segmento al hacer clic
+        });
+        // Capturar el puntero en la escena
+        this.scene.input.on("pointermove", (pointer) => {
+            this.pointermove = { x: pointer.x, y: pointer.y }; // Almacena la posición del puntero
         });
 
         // Configura el evento de liberación del clic para controlar cuando se deja de hacer clic
         this.scene.input.on("pointerup", () => {
             this.isClicking = false; // No se está haciendo clic
-            if (this.pointA && this.pointB) {
-                this.segments.push(new Phaser.Geom.Line(this.pointA.x, this.pointA.y, this.pointB.x, this.pointB.y));
-            }
-            this.pointA = null;
-            this.pointB = null;
+            this.draggingPoint = null;
         });
     }
-    addName() {
-        this.scene.elementNames.push("Segment"); // Agrega el nombre al array de nombres de elementos en la escena
-    }
+
     createSegment() {
-        // Itera a través de los puntos interactivos en la escena
+
         for (const interactivePoint of this.scene.interactivePoints) {
-            if (Phaser.Geom.Rectangle.ContainsPoint(interactivePoint.area, this.scene.input.activePointer)) {
-                // Cambia el aspecto visual del punto interactivo
-                interactivePoint.point.clear();
-                interactivePoint.point.fillStyle(0x00ff00); // Cambia el color a verde
-                interactivePoint.point.fillCircle(
-                    interactivePoint.x,
-                    interactivePoint.y,
-                    5
-                );
-                
-                if (!this.pointA) {
-                    this.pointA = new Phaser.Geom.Point(interactivePoint.x, interactivePoint.y);
-                } else {
-                    this.pointB = new Phaser.Geom.Point(interactivePoint.x, interactivePoint.y);
+            if (Phaser.Geom.Rectangle.ContainsPoint(interactivePoint.area, this.pointermove)) {
+                if (this.pointB == null) {
+                    this.pointA = interactivePoint;
+                    this.pointA.point.fillStyle(0x732c02);
+                    this.pointA.point.fillCircle(
+                        this.pointA.x,
+                        this.pointA.y,
+                        5
+                    );
+                    if (this.isClicking) {
+                        this.pointB.x = this.pointermove.x + interactivePoint.x;
+                        this.pointB.y = this.pointermove.y - interactivePoint.y;
+                        // Actualiza el aspecto visual del punto mientras se mueve
+                        this.segment = this.scene.add.graphics();
+                        this.segment.clear();
+                        this.segment = this.scene.add.graphics({ lineStyle: { width: 2, color: 0xaa00aa } });
+                        this.line = new Phaser.Geom.Line(
+                            this.pointB.x,
+                            this.pointB.y,
+                            this.pointA.x,
+                            this.pointA.y
+                        );
+                        console.log(Phaser.Math.Distance.BetweenPoints(this.pointA.point, interactivePoint.point))
+                    }
+                    break; // Sal del bucle una vez que se haya encontrado el punto A
                 }
             }
         }
+        // Itera a través de los puntos interactivos en la escena
+        for (const interactivePoint of this.scene.interactivePoints) {
+            // Verifica si el puntero se encuentra dentro del área del punto interactivo
+            if (Phaser.Geom.Rectangle.ContainsPoint(interactivePoint.area, this.pointermove)) {
+                // Verifica si el usuario está haciendo clic
+                if (this.isClicking && this.draggingPoint == interactivePoint) {
+                    console.log("x inicial " + this.pointA.x);
+                    this.pointB.x = this.pointermove.x - this.draggingOffsetX;
+                    this.pointB.point.x = this.pointB.x
+                    this.pointB.y = this.pointermove.y - this.draggingOffsetY;
+                    this.pointB.point.y = this.pointB.y
+                    // Actualiza la posición del punto interactivo
+                    interactivePoint.x = this.pointB.x;
+                    interactivePoint.y = this.pointB.y;
+                    interactivePoint.area.setPosition(this.pointB.x - 10, this.pointB.y - 8);
+
+
+
+                    this.segment.strokeLineShape(this.line);
+                }
+
+            }
+            else {
+                // Si el puntero no está sobre el punto interactivo, restaura su aspecto original
+                interactivePoint.point.clear();
+                interactivePoint.point.fillStyle(0x732c02); // Cambia el color al original
+                interactivePoint.point.fillCircle(interactivePoint.x, interactivePoint.y, 5);
+            }
+        } this.pointA = this.initialpointA;
+
     }
+
 }
