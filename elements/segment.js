ac2dc98dbf00348f8cc6bce53d15453123bea783
Jeidsgn <jeirm8228@gmail.com>
2023-08-17 20:26:05 -0500

    create segment

--- elements/segment.js
+++ elements/segment.js
@@ -52,7 +52,7 @@ export class Segment {
                     }
                 }
             } else {
-                if (this.isClicking && this.scene.pointB.x !== undefined) {
+                if (this.isClicking && this.scene.pointB.x) {
                     console.log(this.scene.pointA.x);
                     this.scene.pointB.x = this.pointermove.x + interactivePoint.x;
                     this.scene.pointB.y = this.pointermove.y + interactivePoint.y;
