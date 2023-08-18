e327b6967c5ab23c537d2b16d4d2ee10386f5bb1
Jeidsgn <jeirm8228@gmail.com>
2023-08-17 20:13:19 -0500

    create segment

--- elements/segment.js
+++ elements/segment.js
@@ -13,7 +13,7 @@ export class Segment {
         this.pointermove = { x: 0, y: 0 }; // Almacena la posición del puntero
 
         // Configura el evento de clic en la escena para capturar el puntero
-        this.scene.input.on("pointerdown", (pointer) => {
+        this.scene.input.on("pointerdown", () => {
             this.isClicking = true; // Se está haciendo clic
         });
         // Capturar el puntero en la escena
@@ -34,6 +34,7 @@ export class Segment {
                 if (this.scene.pointA == null) {
                     //console.log("this.scene.pointB == null");
                     this.scene.pointA = interactivePoint;
+                    this.scene.pointB = this.pointermove;
                     this.scene.pointA.point.fillStyle(0x732c02);
                     this.scene.pointA.point.fillCircle(
                         this.scene.pointA.x,
@@ -46,20 +47,22 @@ export class Segment {
                     console.log("this.scene.pointB =! null");
                     if (this.isClicking) {
                         console.log("isClicking");
-                        this.scene.pointB = this.pointermove.x;
+                        this.scene.pointB.x = this.pointermove.x + interactivePoint.x;
+                        this.scene.pointB.y = this.pointermove.y + interactivePoint.y;
                     }
                 }
             } else {
                 if (this.isClicking && this.scene.pointA != null) {
                     console.log(this.scene.pointA.x);
-                    this.scene.pointB = this.pointermove;
+                    this.scene.pointB.x = this.pointermove.x + interactivePoint.x;
+                    this.scene.pointB.y = this.pointermove.y + interactivePoint.y;
                     // Actualiza el aspecto visual del punto mientras se mueve
                     this.segment = this.scene.add.graphics();
                     this.segment.clear();
                     this.segment = this.scene.add.graphics({ lineStyle: { width: 2, color: 0xaa00aa } });
                     this.line = new Phaser.Geom.Line(
-                        this.scene.pointB.point.x,
-                        this.scene.pointB.point.y,
+                        this.scene.pointB.x,
+                        this.scene.pointB.y,
                         this.scene.pointA.x,
                         this.scene.pointA.y
                     );
