d70b408f95d531bfb652083a7e4bc3157f84d426
Jeidsgn <jeirm8228@gmail.com>
2023-08-19 00:07:50 -0500

    create segment

--- elements/segment.js
+++ elements/segment.js
@@ -28,30 +28,32 @@ export class Segment {
             this.isClicking = false; // No se está haciendo clic
         });
     }
-    drawParabola(x1, y1, x2, y2, n) {
-        if (x1 !== x2) {
-            this.graphics.clear(); // Borra cualquier dibujo anterior
-            
-            this.graphics.lineStyle(5, 0x000000); // Estilo de línea
-            
-            // Calcula los coeficientes a, b y c según la fórmula dada
-            const a = (4 * n) / ((x1 - x2) ** 2);
-            const b = (-4 * n * (x1 + x2) + (x1 - x2) * (y1 - y2)) / ((x1 - x2) ** 2);
-            const c = (4 * n * x1 * x2 + (x1 - x2) * (-x2 * y1 + x1 * y2)) / ((x1 - x2) ** 2);
-            
-            const startY = Math.min(y1, y2); // Asegúrate de comenzar desde el punto más bajo
-            
-            this.graphics.moveTo(x1, startY); // Mueve el lápiz al primer punto
-            
-            // Dibuja la parábola utilizando la ecuación y = ax^2 + bx + c
-            for (let x = x1; x <= x2; x++) {
-                const y = a * x ** 2 + b * x + c;
-                this.graphics.lineTo(x, y);
-            }
-            
-            this.graphics.strokePath(); // Dibuja la parábola completa
+// Supongamos que ya tienes una escena de Phaser configurada y has inicializado this.graphics adecuadamente.
+
+drawParabola(x2, y2, x1, y1, n) {
+    if (x1 !== x2) {
+        this.graphics.clear(); // Borra cualquier dibujo anterior
+
+        this.graphics.lineStyle(5, 0x000000, 0.5); // Estilo de línea
+
+        const a = (4 * n) / Math.pow(x1 - x2, 2);
+        const b = (-4 * n * (x1 + x2) + (x1 - x2) * (y1 - y2)) / Math.pow(x1 - x2, 2);
+        const c = (4 * n * x1 * x2 + (x1 - x2) * (-x2 * y1 + x1 * y2)) / Math.pow(x1 - x2, 2);
+
+        const startY = Math.min(y1, y2); // Asegúrate de comenzar desde el punto más bajo
+
+        this.graphics.moveTo(x1, startY); // Mueve el lápiz al primer punto
+
+        // Dibuja la parábola utilizando la ecuación y = ax^2 + bx + c
+        for (let x = x1; x <= x2; x++) {
+            const y = a * x * x + b * x + c;
+            this.graphics.lineTo(x, y);
         }
+
+        this.graphics.strokePath(); // Dibuja la parábola completa
     }
+}
+
     
     
     
