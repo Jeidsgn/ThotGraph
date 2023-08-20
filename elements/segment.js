997339c39bfacb0d404f96150a7e5d46779d00da
Jeidsgn <jeirm8228@gmail.com>
2023-08-20 03:33:28 -0500

    create segment

--- elements/segment.js
+++ elements/segment.js
@@ -81,67 +81,6 @@ export class Segment {
     }
 
 
-    createSegment2() {
-        this.point.stopMovePoint();
-        const interactive = this.scene.points.getChildren();
-        //Se revisan todos los puntos hechos
-        for (const point of interactive) {
-            //Puntero sobre el area del puntos
-            if (Phaser.Geom.Rectangle.ContainsPoint(point.area, this.pointermove)) {
-                //Si no hay seleccionado
-                if (this.scene.pointA == null) {
-                    //se agrega el punto fijo A y se crea el puntoB
-                    this.scene.pointA = point;
-                    this.scene.pointB = this.pointermove;
-                }//si hace clic se sigue el cursor
-                else if (this.isClicking) {
-                    this.scene.pointB = this.pointermove;
-                }
-            }
-            else {//Si hay clic y hay B
-                if (this.scene.pointB !== null) {
-                    if (this.isClicking == true) {
-                        this.scene.pointB = this.pointermove;
-                        // Borrar la línea anterior
-                        this.graphics.clear();
-                        // Actualiza el aspecto visual de la líne mientras se mueve
-                        this.graphics.lineStyle(5, 0x2AA4BF, 0.05);
-                        const line = new Phaser.Geom.Line(
-                            this.scene.pointA.x,
-                            this.scene.pointA.y,
-                            this.scene.pointB.x,
-                            this.scene.pointB.y
-                        );
-                        this.drawParabola(
-                            this.scene.pointA.x,
-                            this.scene.pointA.y,
-                            this.scene.pointB.x,
-                            this.scene.pointB.y,
-                            -20 //Distancia de "caida"
-                        );
-                        this.graphics.strokeLineShape(line);
-                    } else {
-                        this.curve.clear();
-                        this.graphics.clear();
-                        this.graphics.lineStyle(5, 0x2AA4BF, 0.9);
-                        const line = new Phaser.Geom.Line(
-                            this.scene.pointA.x,
-                            this.scene.pointA.y,
-                            this.scene.pointB.x,
-                            this.scene.pointB.y
-                        );
-                        this.graphics.strokeLineShape(line);
-                        this.scene.pointB = null;
-                        this.scene.pointA = null;
-                    }
-                }
-            }
-        }
-        if (!this.isClicking) {
-            this.scene.pointB = null;
-            this.scene.pointA = null;
-        }
-    }
     addName() {
         this.scene.elementNames.push("Segment"); // Agrega el nombre "Point" al array de nombres de elementos en la escena
     }
