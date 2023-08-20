6a20ac0f4a3ccb0bede472ee4d566ec09d437020
Jeidsgn <jeirm8228@gmail.com>
2023-08-20 08:12:29 -0500

    create segment

--- js/Board.js
+++ js/Board.js
@@ -4,7 +4,7 @@ import { Element } from "../elements/Elements.js";
 export class Board extends Phaser.Scene {
   constructor() {
     super({ key: "scene" });
-    
+
     // Propiedades para controlar el estado de la interacción en el tablero
     this.waitingForClick = true;
     this.isDrawingEnabled = false;
@@ -34,42 +34,46 @@ export class Board extends Phaser.Scene {
     this.toolbox.createToolbox();
     // Establece el color de fondo
     const background = this.add.image(
-        this.cameras.main.width / 2,
-        this.cameras.main.height / 2,
-        'Bg'
-      );
+      this.cameras.main.width / 2,
+      this.cameras.main.height / 2,
+      'Bg'
+    );
     background.setDepth(-1);
   }
 
   // Función de actualización que se ejecuta en cada frame
   update() {
-    
+
     if (this.isDrawingEnabled && !this.waitingForClick) {
-        // Llama a la función activa correspondiente
-        if (this.activeButtonCallback) {  // Comprobamos si la función está definida
-          this.activeButtonCallback();  // Ejecutamos la función activa
-        } else {
-          console.log("Error en activeButtonCallback");
-        }
-      } else if (this.isDrawingEnabled && this.waitingForClick) {
-        // Si el dibujo está habilitado y se espera un clic, marca que ya no se espera más
-        this.input.on("pointerdown", () => {
-            this.waitingForClick = false;
-          });
+      // Llama a la función activa correspondiente
+      if (this.activeButtonCallback) {  // Comprobamos si la función está definida
+        this.activeButtonCallback();  // Ejecutamos la función activa
+      } else {
+        console.log("Error en activeButtonCallback");
       }
-      //cuerda vibrante
-    if (this.parabolic != null){
-      this.count +=0.3;
+    } else if (this.isDrawingEnabled && this.waitingForClick) {
+      // Si el dibujo está habilitado y se espera un clic, marca que ya no se espera más
+      this.input.on("pointerdown", () => {
+        this.waitingForClick = false;
+      });
+    }
+    //cuerda vibrante
+    if (this.parabolic != null) {
+      this.count += 0.3;
       let points = this.parabolic.getSpacedPoints(10);
-          for (let i = 1; i < points.length-1; i++){
-              points[i].x += Math.cos(i * 0.5 + this.count);
-              this.curvestyle.lineTo((v+x), y);
-          }  
-          this.curvestyle.strokePath();
-      };
+      for (let i = 1; i < points.length - 1; i++) {
+        points[i].x += Math.cos(i * 0.5 + this.count);
+        if (i === 1) {
+          this.curve.moveTo(points[i].x, points[i].y);
+        } else {
+          this.curve.lineTo(points[i].x, points[i].y);
+        }
+      }
+      this.curvestyle.strokePath();
+    };
     // Configura la función de clic en el contenedor (tablero)
     // Lógica de actualización común, si es necesario
   }
 
-  
+
 }
