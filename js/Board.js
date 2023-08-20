ef6480321977f41b9d75d332515f3b3de5c2fdff
Jeidsgn <jeirm8228@gmail.com>
2023-08-20 09:54:41 -0500

    create segment

--- js/Board.js
+++ js/Board.js
@@ -63,13 +63,13 @@ export class Board extends Phaser.Scene {
     if (this.parabolic != null) {
       console.log("this.parabolic != null")
       this.count += 0.3;
-      let points = this.parabolic.getSpacedPoints(50);
+      let points = this.parabolic.getSpacedPoints(20);
       this.curvestyle.clear(); // Limpia el dibujo anterior
       this.curvestyle.lineStyle(5, 0x2AA4BF, 0.8); // Configura el estilo de línea
       this.path = new Phaser.Curves.Path(points[0].x, points[0].y);
       for (let i = 1; i < points.length - 1; i++) {
         console.log(points[i].x)
-        points[i].x += Math.cos(i * 2 + this.count);   
+        points[i].y += Math.cos(i * 2 + this.count);   
         console.log(i * 0.5 + this.count)     
         this.path.lineTo(points[i].x, points[i].y);
       };
