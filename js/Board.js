00f758c8064cc1d2d4c720be2025e9dacd4f9b9b
Jeidsgn <jeirm8228@gmail.com>
2023-08-20 12:44:17 -0500

    create segment

--- js/Board.js
+++ js/Board.js
@@ -62,7 +62,7 @@ export class Board extends Phaser.Scene {
     //cuerda vibrante
     if (this.parabolic != null) {
       this.count += 0.15;
-      let points = this.parabolic.getSpacedPoints(10);
+      let points = this.parabolic.getSpacedPoints(12);
       this.curvestyle.clear(); // Limpia el dibujo anterior
       this.curvestyle.lineStyle(5, 0x2AA4BF, 0.8); // Configura el estilo de línea
       this.path = new Phaser.Curves.Path(points[0].x, points[0].y);
