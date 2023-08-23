cf405baf28aa0e6b29f541498f94a591a1dab624
Jeidsgn <jeirm8228@gmail.com>
2023-08-23 13:16:33 -0500

    segment

--- elements/segment.js
+++ elements/segment.js
@@ -121,17 +121,15 @@ export class Segment {
         //this.scene.pointdraggable
         for (const segment of this.scene.segments) {
             if (segment.x1 == this.scene.pointdraggable.x) {
-                this.segment_gr.clear();
                 console.log("hola");
-                segment.x1 = this.scene.pointdraggable.x;
-                segment.y1 = this.scene.pointdraggable.y;
+                segment = new Phaser.Geom.Line(this.scene.input.pointer.x, this.scene.input.pointer.y, segment.x2, segment.y2);
                 this.segment_gr.lineStyle(5, 0x2AA4BF, 0.9);
                 this.segment_gr.strokeLineShape(segment);
             } else if (segment.x2 == this.scene.pointdraggable.x) {
                 this.segment_gr.clear();
                 console.log("hola")
-                segment.x2 = this.scene.pointdraggable.x;
-                segment.y2 = this.scene.pointdraggable.y;
+                segment = new Phaser.Geom.Line( segment.x1, segment.y1, this.scene.input.pointer.x, this.scene.input.pointer.y,);
+                this.segment_gr.lineStyle(5, 0x2AA4BF, 0.9);
                 this.segment_gr.strokeLineShape(segment);
             }
         }
