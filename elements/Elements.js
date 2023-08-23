4f87a157d9f391f0161262310df5768637ebbd2b
Jeidsgn <jeirm8228@gmail.com>
2023-08-23 05:45:14 -0500

    buttons

--- elements/Elements.js
+++ elements/Elements.js
@@ -35,6 +35,7 @@ export class Element {
   moveElement() {
     // Crea un nuevo punto utilizando el método "createPoint" de la instancia de Point en la escena
     this.point.movePoint();
+    this.segment.moveSegment();
   }
 
     // Puedes agregar métodos comunes a todos los elementos aquí
