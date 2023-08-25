425ea802e6dfb50956306361aa8a145e38c9c9b8
Jeidsgn <jeirm8228@gmail.com>
2023-08-25 09:11:29 -0500

    segment

--- elements/Elements.js
+++ elements/Elements.js
@@ -21,6 +21,7 @@ export class Element {
       },
       "Segment": () => {
         this.segment.createSegment();
+        this.scene.isCreatingSegment == true;
       },
     };
     return functions[buttonName];
