f913abaf0a3ad572dd72e521f0392e12ecfb504a
Jeidsgn <jeirm8228@gmail.com>
2023-08-27 07:11:28 -0500

    segment

--- elements/point.js
+++ elements/point.js
@@ -4,7 +4,7 @@ export class Point {
         this.count = 1;
         this.scene.pointdraggable = [];
         this.scene.points = scene.add.group(); // Grupo para almacenar los puntos en la escena
-         // Contenedor de texto para las letras de los puntos
+        // Contenedor de texto para las letras de los puntos
         this.isClicking = false; // Variable para controlar si se está haciendo clic
         this.scene.input.on("pointerdown", () => {
             this.isClicking = true; // Se está haciendo clic
@@ -25,32 +25,39 @@ export class Point {
             const letter = this.count;
             this.count = this.count + 1;
             // Crea la imagen del punto en las coordenadas del clic
-            const point = this.scene.add.sprite(this.pointer.x, this.pointer.y, 'point', 0).setOrigin(0.5, 0.80);
-            this.textContainer = this.scene.add.text(point.x, point.y-26, "", { fill: "#000000" });
+            const point = this.scene.add
+                .sprite(this.pointer.x, this.pointer.y, "point", 0)
+                .setOrigin(0.5, 0.8);
+            this.textContainer = this.scene.add.text(point.x, point.y - 26, "", {
+                fill: "#000000",
+            });
             this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
             point.id = letter; // Agrega el nombre del punto
-            point.setData('vector', (this.pointer.x, this.pointer.y));
+            point.setData("vector", (this.pointer.x, this.pointer.y));
             this.scene.points.add(point); // Agrega el punto al grupo
             this.isClicking = false; // Desactiva el clic para evitar creación continua en el mismo clic
         }
     }
-    movePoint(){
+    movePoint() {
         if (this.scene.activatebutton == "Move") {
-            let draggingPoint = null; 
-            const interactive = this.scene.points.getChildren();//
+            let draggingPoint = null;
+            const interactive = this.scene.points.getChildren(); //
             for (const point of interactive) {
-              point.on("pointerdown", () => {
-                if (this.scene.activatebutton == "Move") {
-                  draggingPoint = point;
-                }
-              });
-              point.on("drag", (pointer, dragX, dragY) => {
-                if (draggingPoint === point) {
-                  if (this.scene.activatebutton == "Move") {
-                    this.scene.segment_gr.clear();
-                    point.x = dragX;
-                    point.y = dragY;}}})
+                point.on("pointerdown", () => {
+                    if (this.scene.activatebutton == "Move") {
+                        draggingPoint = point;
+                    }
+                });
+                point.on("drag", (pointer, dragX, dragY) => {
+                    if (draggingPoint === point) {
+                        if (this.scene.activatebutton == "Move") {
+                            this.scene.segment_gr.clear();
+                            point.x = dragX;
+                            point.y = dragY;
+                        }
+                    }
+                });
+            }
         }
     }
-
 }
