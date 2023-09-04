171a91e42536be2bf870a5022a39b9c46c4e2488
Jeidsgn <jeirm8228@gmail.com>
2023-09-04 17:27:12 -0500

    circle

--- elements/circle.js
+++ elements/circle.js
@@ -100,11 +100,8 @@ export class Circle {
                                 this.scene.circles_gr[i].lineStyle(5, 0xF250DA, 0.9);
                                 circle.p0.x = point.x;
                                 circle.p0.y = point.y;
-                                let ratio = Phaser.Math.Distance.Between(circle.x, circle.y, circle.p1.x, circle.p1.y)
-                                circle.x = circle.p0.x
-                                circle.y = circle.p0.y
-                                console.log(ratio)
-                                circle.width = ratio;
+                                circle.xRadius = circle.p0.x
+                                circle.yRadius = circle.p0.y
                                 circle.draw(this.scene.circles_gr[i]);
                                                             // Actualiza los puntos internos asociados al segmento
                             for (const innerpoint of circle.innerpoint) {
@@ -116,9 +113,8 @@ export class Circle {
                             } else if (point == circle.p1) {
                                 this.scene.circles_gr[i].clear();
                                 this.scene.circles_gr[i].lineStyle(5, 0xF250DA, 0.9);
-                                let ratio = Phaser.Math.Distance.Between(circle.x, circle.y, point.x, point.y)
-                                console.log(ratio)
-                                circle.width = ratio;
+                                circle.xRadius = point.x;
+                                circle.yRadius = point.y;
                                 circle.draw(this.scene.circles_gr[i]);
 
                                                             // Actualiza los puntos internos asociados al segmento
