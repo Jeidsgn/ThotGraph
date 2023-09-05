b00f478ab7120ff64dab43be8a729e8668d1d9ec
Jeidsgn <jeirm8228@gmail.com>
2023-09-04 23:47:29 -0500

    circle

--- elements/point.js
+++ elements/point.js
@@ -9,6 +9,7 @@ export class Point {
         this.coordenates = null;
         this.scene.input.on("pointerdown", () => {
             this.isClicking = true; // Se está haciendo clic
+            this.pointCreated = false; // No hay punto creado
         });
         this.scene.input.on("pointermove", (pointer) => {
             this.pointer = pointer; // No se está haciendo clic
@@ -21,14 +22,14 @@ export class Point {
         this.scene.elementNames.push("Point"); // Agrega el nombre "Point" al array de nombres de elementos en la escena
     }
     getNearestPointOnSegment(p0, p1, p) {
-        let line = new Phaser.Geom.Line(p0.x,p0.y,p1.x,p1.y);
+        let line = new Phaser.Geom.Line(p0.x, p0.y, p1.x, p1.y);
         let np = Phaser.Geom.Line.GetNearestPoint(line, new Phaser.Geom.Point(p.x, p.y));
         return np;
-        
+
     }
-    
+
     createPoint() {
-        if (this.isClicking) {
+        if (this.isClicking && !this.pointCreated) {
             const letter = this.count;
             this.count = this.count + 1;
             // Inicializa variables para rastrear la línea y el punto más cercano
@@ -54,8 +55,47 @@ export class Point {
                     nearsegment = segment;
                     nearpoint = pointsegment;
                 }
-                proportion = (nearpoint.x-nearsegment.p0.x)/(nearsegment.p1.x-nearsegment.p0.x);
+                proportion = (nearpoint.x - nearsegment.p0.x) / (nearsegment.p1.x - nearsegment.p0.x);
                 this.coordenates = nearsegment.getPointAt(proportion);
+                // Si la distancia es menor a 15 píxeles, crea el punto en el punto más cercano en la línea
+                if (neardistance < 15) {
+
+                    const point = this.scene.add
+                        .sprite(this.coordenates.x, this.coordenates.y, "point", 0)
+                        .setOrigin(0.5, 0.58);
+                    this.textContainer = this.scene.add.text(point.x, point.y - 26, "", {
+                        fill: "#000000",
+                    });
+                    // Asigna el segmento al punto
+                    point.segment = nearsegment;
+                    nearsegment.innerpoint.push(point);
+                    this.textContainer.text += letter + " "; // Agrega la letra asociada al punto al contenedor de texto
+                    point.id = letter; // Agrega el nombre del punto
+                    point.setData("t", proportion);
+                    this.scene.points.add(point); // Agrega el punto al grupo
+                    this.isClicking = false; // Desactiva el clic para evitar creación continua en el mismo clic
+                    this.pointCreated = true; // Establece la bandera para indicar que se ha creado un punto
+                }
+            }
+            // Itera a través de los circulos y encuentra el más cercana
+            for (let i = 0; i < this.scene.circles.length; i++) {
+                let segment = this.scene.segments[i];
+                let pointsegment = this.getNearestPointOnSegment(segment.p0, segment.p1, this.pointer);
+                let distance = Phaser.Math.Distance.Between(
+                    this.pointer.x,
+                    this.pointer.y,
+                    pointsegment.x,
+                    pointsegment.y
+                );
+
+                if (distance < neardistance) {
+                    neardistance = distance;
+                    nearsegment = segment;
+                    nearpoint = pointsegment;
+                }
+                proportion = (nearpoint.x - nearsegment.p0.x) / (nearsegment.p1.x - nearsegment.p0.x);
+                this.coordenates = nearsegment.getPointAt(proportion);
+                // Si la distancia es menor a 15 píxeles, crea el punto en el punto más cercano en la línea
                 if (neardistance < 15) {
 
                     const point = this.scene.add
@@ -74,8 +114,8 @@ export class Point {
                     this.isClicking = false; // Desactiva el clic para evitar creación continua en el mismo clic
                 }
             }
-            // Si la distancia es menor a 15 píxeles, crea el punto en el punto más cercano en la línea
-            
+
+
         }
     }
     movePoint() {
@@ -87,15 +127,15 @@ export class Point {
                     if (this.scene.activatebutton === "Move") {
                         // Obtén el segmento al que pertenece el punto
                         const segment = point.segment;
-                        
+
                         if (segment) {
                             // Calcula la posición relativa 't' dentro del segmento
-                            let t = (dragX-segment.p0.x)/(segment.p1.x-segment.p0.x);
-                            t = Phaser.Math.Clamp(t,0,1);
-                            
+                            let t = (dragX - segment.p0.x) / (segment.p1.x - segment.p0.x);
+                            t = Phaser.Math.Clamp(t, 0, 1);
+
                             // Calcula las coordenadas del punto en el segmento
                             const { x, y } = segment.getPointAt(t);
-                            
+
                             // Actualiza la posición del punto solo dentro del segmento
                             point.x = x;
                             point.y = y;
@@ -106,5 +146,5 @@ export class Point {
             }
         }
     }
-    
+
 }
