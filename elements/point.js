4371ace0291434d304e40466fa58861dc4af86fa
Jeidsgn <jeirm8228@gmail.com>
2023-09-07 15:18:20 -0500

    intersection

--- elements/point.js
+++ elements/point.js
@@ -149,7 +149,6 @@ export class Point {
                         nearpoint = pointline;
                     }
                     proportion = (nearpoint.x - nearline.p0.x) / (nearline.p1.x - nearline.p0.x);
-                    console.log(proportion);
                     this.coordenates = nearline.getPointAt(proportion);
                     // Si la distancia es menor a 15 píxeles, crea el punto en el punto más cercano en la línea+
                     
@@ -190,6 +189,7 @@ export class Point {
                     }
                     proportion = (nearpoint.x - nearsegment.p0.x) / (nearsegment.p1.x - nearsegment.p0.x);
                     this.coordenates = nearsegment.getPointAt(proportion);
+                    console.log(proportion);
                     // Si la distancia es menor a 15 píxeles, crea el punto en el punto más cercano en la línea
                     if (NearDistanceSegment < 15 && this.pointscreated == this.scene.points.getChildren().length) {
                         const point = this.scene.add
